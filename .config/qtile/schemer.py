b4c0db31f9293b1a1001336eda078a7752177676
Graphity <rohood3@gmail.com>
2020-04-15 17:26:31 +0400

    Slight change of one color

--- .config/qtile/schemer.py
+++ .config/qtile/schemer.py
@@ -1,6 +1,7 @@
 colors = {
-    'M': '2c2e2f',  # gray
-    'm': '222222',  # dark gray
+    'M': '2e3442',  # gray
+    'm': '2e3442',  # dark gray
     'S': '1f388a',  # blue
     's': '45494e'   # graphite
 }
+
